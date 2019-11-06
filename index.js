caabd66234ba38f9517f5af4897ff7e09001634d
Luqman Olushi <olushilukmon03@gmail.com>
2019-11-06 11:28:37 +0100

    add lib

---
+++ index.js
@@ -0,0 +1,64 @@
+import React, { Fragment, useState, useEffect, useRef } from 'react';
+import IdleTimer from 'react-idle-timer';
+import PropTypes from 'prop-types';
+
+function LockScreen(props) {
+  const [isLocked, setLock] = useState(false);
+  const init = useRef(0);
+
+  const handleOnIdle = () => {
+    if (!isLocked) {
+      setLock(!isLocked);
+    }
+  };
+
+  useEffect(() => {
+    if (!init.current) {
+      init.current = 1;
+      return;
+    }
+    isLocked ? props.onScreenLocked() : props.onScreenUnlocked();
+  }, [isLocked]);
+
+  const renderLockScreen = () => {
+    if (isLocked) return props.lockScreen(setLock);
+  };
+
+  const defaultLockScreenStyles = () =>
+    isLocked && !props.className
+      ? {
+          filter: 'blur(4px)',
+          userSelect: 'none',
+          height: '100vh',
+          overflow: 'hidden'
+        }
+      : {};
+
+  return (
+    <Fragment>
+      <IdleTimer onIdle={handleOnIdle} timeout={props.timeout} />
+      <div
+        style={defaultLockScreenStyles()}
+        className={`react-lock-screen ${isLocked ? props.className : ''}`}
+      >
+        {props.children}
+      </div>
+      {renderLockScreen()}
+    </Fragment>
+  );
+}
+
+LockScreen.defaultProps = {
+  onScreenLocked: () => {},
+  onScreenUnlocked: () => {}
+};
+
+LockScreen.propTypes = {
+  timeout: PropTypes.number.isRequired,
+  lockScreen: PropTypes.func.isRequired,
+  className: PropTypes.string,
+  onScreenLocked: PropTypes.func,
+  onScreenUnlocked: PropTypes.func
+};
+
+export default LockScreen;
